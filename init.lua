849a5ad1489d546228ea6aa37939b822042c9323
Alikhan Bissakov <78418192+bissakov@users.noreply.github.com>
2024-09-07 20:11:04 +0500

    (feat): Remove 'Mofiqul/vscode.nvim'

--- init.lua
+++ init.lua
@@ -314,16 +314,6 @@ require('lazy').setup({
         vim.cmd.hi 'Comment gui=none'
       end,
     },
-    {
-      'Mofiqul/vscode.nvim',
-      priority = 1000,
-      init = function()
-        -- require('vscode').setup()
-
-        -- vim.cmd.colorscheme 'vscode'
-        -- vim.cmd.hi 'Comment gui=none'
-      end,
-    },
   },
   {
     'folke/lazydev.nvim',
